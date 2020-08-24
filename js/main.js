1b1216cd3f4c5f4cf1da7fbe7521f0bab576b469
ArqMartinB <63083550+ArqMartinB@users.noreply.github.com>
2020-08-24 16:22:11 -0300

    Update main.js
    fix decimal

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function inversion ()
  {
     var cotizacionblue=Number(document.getElementById('blue').value);
     var dolares=Number(document.getElementById('venta').value);
-    var inv=dolares*cotizacionblue;
+    var inv=(dolares*cotizacionblue).toFixed(3);
     document.getElementById('inv').value=inv;
 }
 
@@ -12,8 +12,8 @@ function resultado ()
     var daiventa=Number(document.getElementById('daiusd').value);
     var cotizacionblue=Number(document.getElementById('blue').value);
     var dolares=Number(document.getElementById('venta').value);
-    var inv=dolares*cotizacionblue;
-    var res=inv/daicompra*daiventa;
+    var inv=(dolares*cotizacionblue).toFixed(3);
+    var res=(inv/daicompra*daiventa).toFixed(3);
     document.getElementById('res').value=res; 
 }
 
@@ -21,6 +21,7 @@ function cambio ()
 {
     var daicompra=Number(document.getElementById('daiarg').value);
     var daiventa=Number(document.getElementById('daiusd').value);
-    var camb=daicompra/daiventa;
+    var camb=(daicompra/daiventa).toFixed(3);
     document.getElementById('camb').value=camb; 
 }
+
